68bd0961833d58edb41401460ee9ea5287ae9271
Renyuan Lyu <renyuan.lyu@gmail.com>
2023-08-16 10:11:15 +0800

    ....

---
+++ ch01/ch01.py
@@ -0,0 +1,201 @@
+# %% [markdown]
+# # Machine Learning with PyTorch and Scikit-Learn  
+# # -- Code Examples
+
+# %% [markdown]
+# ## Package version checks
+
+# %% [markdown]
+# Add folder to path in order to load from the check_packages.py script:
+
+# %%
+import sys
+sys.path.insert(0, '..')
+
+# %% [markdown]
+# Check recommended package versions:
+
+# %%
+from python_environment_check import check_packages
+
+
+d = {
+    'numpy': '1.21.2',
+    'scipy': '1.7.0',
+    'matplotlib': '3.4.3',
+    'sklearn': '1.0',
+    'pandas': '1.3.2'
+}
+check_packages(d)
+
+# %% [markdown]
+# # Chapter 1 - Giving Computers the Ability to Learn from Data
+
+# %% [markdown]
+# ### Overview
+
+# %% [markdown]
+# - [Building intelligent machines to transform data into knowledge](#Building-intelligent-machines-to-transform-data-into-knowledge)
+# - [The three different types of machine learning](#The-three-different-types-of-machine-learning)
+#     - [Making predictions about the future with supervised learning](#Making-predictions-about-the-future-with-supervised-learning)
+#         - [Classification for predicting class labels](#Classification-for-predicting-class-labels)
+#         - [Regression for predicting continuous outcomes](#Regression-for-predicting-continuous-outcomes)
+#     - [Solving interactive problems with reinforcement learning](#Solving-interactive-problems-with-reinforcement-learning)
+#     - [Discovering hidden structures with unsupervised learning](#Discovering-hidden-structures-with-unsupervised-learning)
+#         - [Finding subgroups with clustering](#Finding-subgroups-with-clustering)
+#         - [Dimensionality reduction for data compression](#Dimensionality-reduction-for-data-compression)
+#         - [An introduction to the basic terminology and notations](#An-introduction-to-the-basic-terminology-and-notations)
+# - [A roadmap for building machine learning systems](#A-roadmap-for-building-machine-learning-systems)
+#     - [Preprocessing - getting data into shape](#Preprocessing--getting-data-into-shape)
+#     - [Training and selecting a predictive model](#Training-and-selecting-a-predictive-model)
+#     - [Evaluating models and predicting unseen data instances](#Evaluating-models-and-predicting-unseen-data-instances)
+# - [Using Python for machine learning](#Using-Python-for-machine-learning)
+# - [Installing Python packages](#Installing-Python-packages)
+# - [Summary](#Summary)
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %%
+from IPython.display import Image
+
+# %% [markdown]
+# # Building intelligent machines to transform data into knowledge
+
+# %% [markdown]
+# ...
+
+# %% [markdown]
+# # The three different types of machine learning
+
+# %%
+Image(filename='./figures/01_01.png', width=500) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# ## Making predictions about the future with supervised learning
+
+# %%
+Image(filename='./figures/01_02.png', width=500) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# ### Classification for predicting class labels
+
+# %%
+Image(filename='./figures/01_03.png', width=300) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# ### Regression for predicting continuous outcomes
+
+# %%
+Image(filename='./figures/01_04.png', width=300) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# ## Solving interactive problems with reinforcement learning
+
+# %%
+Image(filename='./figures/01_05.png', width=300) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# ## Discovering hidden structures with unsupervised learning
+
+# %% [markdown]
+# ...
+
+# %% [markdown]
+# ### Finding subgroups with clustering
+
+# %%
+Image(filename='./figures/01_06.png', width=300) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# ### Dimensionality reduction for data compression
+
+# %%
+Image(filename='./figures/01_07.png', width=500) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# ### An introduction to the basic terminology and notations
+
+# %%
+Image(filename='./figures/01_08.png', width=500) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# # A roadmap for building machine learning systems
+
+# %%
+Image(filename='./figures/01_09.png', width=700) 
+
+# %% [markdown]
+# <br>
+# <br>
+
+# %% [markdown]
+# ## Preprocessing - getting data into shape
+
+# %% [markdown]
+# ...
+
+# %% [markdown]
+# ## Training and selecting a predictive model
+
+# %% [markdown]
+# ...
+
+# %% [markdown]
+# ## Evaluating models and predicting unseen data instances
+
+# %% [markdown]
+# ...
+
+# %% [markdown]
+# # Using Python for machine learning
+
+# %% [markdown]
+# ...
+
+# %% [markdown]
+# # Installing Python packages
+
+# %% [markdown]
+# ...
+
+# %% [markdown]
+# # Summary
+
+# %% [markdown]
+# ...
+
+
